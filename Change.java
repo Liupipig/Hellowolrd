643a02f19d724235168522ff10164a37e134fd10
Liupipig <154806013+Liupipig@users.noreply.github.com>
2023-12-26 21:51:02 +0800

    Escape characters of java
    java的转义字符：制表符\t、换行\n、单斜杠\\、双引号\"、回车\r

---
+++ Change.java
@@ -0,0 +1,11 @@
+public class Change{
+	public static void main(String[] args){
+		System.out.println("陕西\t汉中\t西安\t成都");
+		System.out.println("水浒传\n红楼梦");
+		System.out.println("Liupipi\\g");
+		System.out.println("Liupipi\\\\g");
+		System.out.println("我说:\"要好好学习\"");
+		System.out.println("我说:\r\n学习");
+
+	}
+}
---
+++ JavaDoccomment.java
@@ -0,0 +1,10 @@
+//文档注释
+/**
+ * @author Mr.Yang
+ * @version 1.0
+ */
+public class JavaDoccomment{
+	public static void main(String[] args){
+
+	}
+}
